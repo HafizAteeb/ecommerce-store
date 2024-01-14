0accbc1783cb46fcdf995ac74c46be899180c1d2
Ateeb Shabbir <hafiz.ateeb@walee.pk>
2024-01-14 23:27:17 +0500

    init

---
+++ app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { ProductColumn, columns } from "./columns";
+import { DataTable } from "@/components/ui/data-table";
+
+interface ProductClientProps {
+  data: ProductColumn[];
+}
+
+export const ProductClient: React.FC<ProductClientProps> = ({ data }) => {
+  return (
+    <>
+      <Heading
+        title={`Orders (${data.length})`}
+        description="Manage orders for your store"
+      />
+      <Separator />
+      <DataTable searchKey="products" columns={columns} data={data} />
+    </>
+  );
+};
---
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -0,0 +1,5 @@
+const DashboardPage = () => {
+  return <div>This is a dasboard</div>;
+};
+
+export default DashboardPage;
